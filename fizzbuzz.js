6408a2217982687db07f6a6f196a2586b77bfffe
aLanois14 <axel.lanois@outlook.fr>
2025-03-03 15:16:19 +0100

    first test

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,8 @@
-function fizzBuzz(n) {
-  if (n % 5 === 0) return "buzz";
-  return n;
+function fizzBuzz(_nombre) {
+  if (_nombre === 0) return "FizzBuzz";
+  if (_nombre === 3 || _nombre === 6 || _nombre === 9) return "Fizz";
+  if (_nombre === 5 || _nombre === 10) return "Buzz";
+  return undefined;
 }
 
 module.exports = fizzBuzz;
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,19 +1,113 @@
 const fizzBuzz = require("./fizzbuzz");
 
-describe("FizzBuzz Tests - Divisibilité par 5 seulement", () => {
-  test("Renvoie 'buzz' si divisible par 5", () => {
-    expect(fizzBuzz(5)).toBe("buzz");
-    expect(fizzBuzz(10)).toBe("buzz");
-    expect(fizzBuzz(15)).toBe("buzz");
-    expect(fizzBuzz(30)).toBe("buzz");
+describe("Fizz", () => {
+  test("0", () => {
+    //GIVEN
+    const nombreDeDepart = 0;
+    const resultatAttendu = "FizzBuzz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
   });
 
-  test("Renvoie le nombre lui-même si non divisible par 5", () => {
-    expect(fizzBuzz(1)).toBe(1);
-    expect(fizzBuzz(2)).toBe(2);
-    expect(fizzBuzz(3)).toBe(3);
-    expect(fizzBuzz(4)).toBe(4);
-    expect(fizzBuzz(6)).toBe(6);
-    expect(fizzBuzz(7)).toBe(7);
+  test("1", () => {
+    //GIVEN
+    const nombreDeDepart = 1;
+    const resultatAttendu = undefined;
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("2", () => {
+    //GIVEN
+    const nombreDeDepart = 2;
+    const resultatAttendu = undefined;
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("3", () => {
+    //GIVEN
+    const nombreDeDepart = 3;
+    const resultatAttendu = "Fizz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("4", () => {
+    //GIVEN
+    const nombreDeDepart = 4;
+    const resultatAttendu = undefined;
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("5", () => {
+    //GIVEN
+    const nombreDeDepart = 5;
+    const resultatAttendu = "Buzz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("6", () => {
+    //GIVEN
+    const nombreDeDepart = 6;
+    const resultatAttendu = "Fizz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("7", () => {
+    //GIVEN
+    const nombreDeDepart = 7;
+    const resultatAttendu = undefined;
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("8", () => {
+    //GIVEN
+    const nombreDeDepart = 8;
+    const resultatAttendu = undefined;
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("9", () => {
+    //GIVEN
+    const nombreDeDepart = 9;
+    const resultatAttendu = "Fizz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
+  });
+
+  test("10", () => {
+    //GIVEN
+    const nombreDeDepart = 10;
+    const resultatAttendu = "Buzz";
+    //WHEN
+    const resultat = fizzBuzz(nombreDeDepart);
+    //THEN
+    expect(resultat).toBe(resultatAttendu);
   });
 });
